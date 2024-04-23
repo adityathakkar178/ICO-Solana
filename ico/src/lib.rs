9c8a86336b157205d5dbecce60b4da25c172680c
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-04-23 17:34:53 +0530

    transfer-add

--- ico/src/lib.rs
+++ ico/src/lib.rs
@@ -1,78 +1,16 @@
-use {
-    borsh::{BorshDeserialize, BorshSerialize},
-    solana_program::{
-        account_info::{next_account_info, AccountInfo},
-        entrypoint,
-        entrypoint::ProgramResult,
-        msg,
-        program::invoke,
-        program_pack::Pack,
-        pubkey::Pubkey,
-        rent::Rent,
-        system_instruction,
-        sysvar::Sysvar,
-    },
-    spl_token::{instruction as token_instruction, state::Mint},
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
 };
 
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct CreateTokenArgs {
-    pub token_decimals: u8,
-}
+pub mod instructions;
+pub mod processor;
 
 entrypoint!(process_instruction);
 
 fn process_instruction(
-    _program_id: &Pubkey,
+    program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    let args = CreateTokenArgs::try_from_slice(instruction_data)?;
-
-    let accounts_iter = &mut accounts.iter();
-
-    let mint_account = next_account_info(accounts_iter)?;
-    let mint_authority = next_account_info(accounts_iter)?;
-    let payer = next_account_info(accounts_iter)?;
-    let rent = next_account_info(accounts_iter)?;
-    let system_program = next_account_info(accounts_iter)?;
-    let token_program = next_account_info(accounts_iter)?;
-
-    msg!("Mint: {}", mint_account.key);
-    invoke(
-        &system_instruction::create_account(
-            payer.key,
-            mint_account.key,
-            (Rent::get()?).minimum_balance(Mint::LEN),
-            Mint::LEN as u64,
-            token_program.key,
-        ),
-        &[
-            mint_account.clone(),
-            payer.clone(),
-            system_program.clone(),
-            token_program.clone(),
-        ],
-    )?;
-
-    msg!("Mint: {}", mint_account.key);
-    invoke(
-        &token_instruction::initialize_mint(
-            token_program.key,
-            mint_account.key,
-            mint_authority.key,
-            Some(mint_authority.key),
-            args.token_decimals,
-        )?,
-        &[
-            mint_account.clone(),
-            mint_authority.clone(),
-            token_program.clone(),
-            rent.clone(),
-        ],
-    )?;    
-
-    msg!("Token mint created successfully.");
-
-    Ok(())
+    processor::process_instruction(program_id, accounts, instruction_data)
 }
--- test/ico.test.js
+++ test/ico.test.js
@@ -1,89 +1,161 @@
 const {
     Connection,
     Keypair,
+    PublicKey,
     SystemProgram,
     SYSVAR_RENT_PUBKEY,
     TransactionInstruction,
-    sendAndConfirmTransaction,
     Transaction,
+    sendAndConfirmTransaction,
 } = require('@solana/web3.js');
 const {
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+    getAssociatedTokenAddress,
     TOKEN_PROGRAM_ID,
 } = require('@solana/spl-token');
-const borsh = require("borsh");
 const { Buffer } = require("buffer");
+const { CreateTokenArgs, MintNftArgs, MintSplArgs, TransferTokensArgs, MyInstruction } = require('./instruction');
+const { BN } = require('bn.js');
+const fs = require('fs');
 
 function createKeypairFromFile(path) {
     return Keypair.fromSecretKey(
-        Buffer.from(JSON.parse(require('fs').readFileSync(path, "utf-8")))
+        Buffer.from(JSON.parse(fs.readFileSync(path, "utf-8")))
     );
 }
 
-class Assignable {
-    constructor(properties) {
-        Object.keys(properties).map((key) => {
-            return (this[key] = properties[key]);
-        });
-    }
-}
+describe("Transferring Tokens",  () => {
 
-class CreateTokenArgs extends Assignable {
-    toBuffer() {
-        return Buffer.from(borsh.serialize(CreateTokenArgsSchema, this));
-    }
-}
-const CreateTokenArgsSchema = new Map([
-    [
-        CreateTokenArgs, {
-            kind: 'struct',
-            fields: [
-                ['token_decimals', 'u8'],
-            ]
-        }
-    ]
-]);
-
-describe("Create Tokens!",  () => {
-
-    const connection = new Connection('https://api.devnet.solana.com', 'confirmed');
+    const connection = new Connection(`https://api.devnet.solana.com/`, 'confirmed');
     const payer = createKeypairFromFile(require('os').homedir() + '/.config/solana/id.json');
     const program = createKeypairFromFile('./ico/target/deploy/ico-keypair.json');
 
-    it("Mint token!",  async () => {
+    const tokenMintKeypair = Keypair.generate();
 
-        const mintKeypair = Keypair.generate();
+    const recipientWallet = Keypair.generate();
+
+    it("Create an SPL Token!",  async () => {
+
+        
 
         const instructionData = new CreateTokenArgs({
-            token_decimals: 9,
+            decimals: 9,
         });
 
         let ix = new TransactionInstruction({
             keys: [
-                { pubkey: mintKeypair.publicKey, isSigner: true, isWritable: true },           
-                { pubkey: payer.publicKey, isSigner: false, isWritable: true },                
-                { pubkey: payer.publicKey, isSigner: true, isWritable: true }, 
-                { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },                 
-                { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },        
-                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },               
+                { pubkey: tokenMintKeypair.publicKey, isSigner: true, isWritable: true },            // Mint account
+                { pubkey: payer.publicKey, isSigner: false, isWritable: true },                 // Mint authority account
+                { pubkey: payer.publicKey, isSigner: true, isWritable: true },                  // Payer
+                { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },             // Rent account
+                { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },        // System program
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },               // Token program
             ],
             programId: program.publicKey,
             data: instructionData.toBuffer(),
         });
+        const ts = new Transaction().add(ix);
+        const sx = await sendAndConfirmTransaction(
+            connection,
+            ts,
+            [payer, tokenMintKeypair]
+        );
 
+        console.log("Success!");
+        console.log(`   Mint Address: ${tokenMintKeypair.publicKey}`);
+        console.log(`   Tx Signature: ${sx}`);
+    });
+
+
+    it("Mint some tokens to your wallet!", async () => {
+
+        const associatedTokenAccountAddress = await getAssociatedTokenAddress(
+            tokenMintKeypair.publicKey,
+            payer.publicKey,
+        );
+
+        const instructionData = new MintSplArgs({
+            instruction: MyInstruction.MintSpl,
+            quantity: new BN(150),
+        });
+
+        let ix = new TransactionInstruction({
+            keys: [
+                { pubkey: tokenMintKeypair.publicKey, isSigner: false, isWritable: true },           // Mint account
+                { pubkey: payer.publicKey, isSigner: false, isWritable: true },                 // Mint authority account
+                { pubkey: associatedTokenAccountAddress, isSigner: false, isWritable: true },   // ATA
+                { pubkey: payer.publicKey, isSigner: true, isWritable: true },                  // Payer
+                { pubkey: SystemProgram.programId, isSigner: false, isWritable: true },                  // System program
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },               // Token program
+                { pubkey: ASSOCIATED_TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },    // Token metadata program
+            ],
+            programId: program.publicKey,
+            data: instructionData.toBuffer(),
+        });
         const ts = new Transaction().add(ix);
 
         const sx = await sendAndConfirmTransaction(
             connection,
             ts,
-            [payer, mintKeypair]
+            [payer],
         );
 
         console.log("Success!");
-        console.log(`Mint Address: ${mintKeypair.publicKey}`);
-        console.log(ix);
-        console.log(ts);
-        console.log(sx);
+        console.log(`   ATA Address: ${associatedTokenAccountAddress}`);
+        console.log(`   Tx Signature: ${sx}`);
     });
 
-});
 
+    it("Prep a new test wallet for transfers", async () => {
+
+        await connection.confirmTransaction(
+            await connection.requestAirdrop(
+                recipientWallet.publicKey,
+                await connection.getMinimumBalanceForRentExemption(0),
+            )
+        );
+        console.log(`Recipient Pubkey: ${recipientWallet.publicKey}`);
+    });
+
+    it("Transfer tokens to another wallet!", async () => {
+
+        const fromAssociatedTokenAddress = await getAssociatedTokenAddress(
+            tokenMintKeypair.publicKey,
+            payer.publicKey
+        );
+        console.log(`Owner Token Address: ${fromAssociatedTokenAddress}`);
+        const toAssociatedTokenAddress = await getAssociatedTokenAddress(
+            tokenMintKeypair.publicKey,
+            recipientWallet.publicKey
+        );
+        console.log(`Recipient Token Address: ${toAssociatedTokenAddress}`);
+
+        const transferToInstructionData = new TransferTokensArgs({
+            instruction: MyInstruction.TransferTokens,
+            quantity: new BN(15),
+        });
+
+        let ix = new TransactionInstruction({
+            keys: [
+                { pubkey: tokenMintKeypair.publicKey, isSigner: false, isWritable: true },      // Mint account
+                { pubkey: fromAssociatedTokenAddress, isSigner: false, isWritable: true },      // Owner Token account
+                { pubkey: toAssociatedTokenAddress, isSigner: false, isWritable: true },        // Recipient Token account
+                { pubkey: payer.publicKey, isSigner: true, isWritable: true },                  // Owner
+                { pubkey: recipientWallet.publicKey, isSigner: true, isWritable: true },        // Recipient
+                { pubkey: payer.publicKey, isSigner: true, isWritable: true },                  // Payer
+                { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },        // System program
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },               // Token program
+                { pubkey: ASSOCIATED_TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },    // Associated token program
+            ],
+            programId: program.publicKey,
+            data: transferToInstructionData.toBuffer(),
+        });
+        const ts = new Transaction().add(ix);
+        await sendAndConfirmTransaction(
+            connection,
+            ts,
+            [payer, recipientWallet],
+            { skipPreflight: true }
+        );
+    });
+});

8344b4a66d9c2cb394d7ba3819a2b95ad5060d17
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-04-24 18:30:58 +0530

    whitelist-start

--- ico/src/instructions/whitelist.rs
+++ ico/src/instructions/whitelist.rs
@@ -4,13 +4,22 @@ use {
         account_info::{next_account_info, AccountInfo},
         entrypoint::ProgramResult,
         msg,
-        program::invoke,
+        program::invoke, 
+        pubkey::Pubkey,
     },
-    spl_associated_token_account::instruction as associated_token_account_instruction,
-    spl_token::instruction as token_instruction, std::string,
+    spl_token::instruction as token_instruction,
 };
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct WhitelistArgs<'a> {
-    pub accounts: Vec<&'a AccountInfo<'a>>,
+pub struct WhitelistArgs {
+    pub accounts: Pubkey,
+    pub is_whitelisted: bool,
+}
+
+pub fn whitelist_account(
+    _args: WhitelistArgs,
+) -> ProgramResult {
+
+
+    Ok(())
 }

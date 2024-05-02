9a8c154be0dc753f0ce6ba91ef98df6758792b25
adityathakkar178 <160093854+adityathakkar178@users.noreply.github.com>
2024-05-02 18:13:39 +0530

    whitelist-adjust

--- ico/src/instructions/whitelist.rs
+++ ico/src/instructions/whitelist.rs
@@ -9,6 +9,8 @@ pub struct WhitelistArgs {
     pub accounts: Vec<String>,
 }
 
+static mut IS_WHITELISTED: Option<String> = None;
+
 const ADMIN_ACCOUNT: &str = "Cv66dQwcSJDdXNVHybchYqp73d75Y8XWj6pKcjN3ffAy";
 
 pub fn whitelist_account(args: WhitelistArgs, admin_account: String) -> ProgramResult {
@@ -39,8 +41,38 @@ pub fn whitelist_account(args: WhitelistArgs, admin_account: String) -> ProgramR
     let proof = whitelist_tree.make_proof(leaf);
     msg!("Proof: {:?}", proof);
 
-    let result = whitelist_tree.check_proof(proof, leaf);
+    let result = verify_proof(whitelist_tree);
     msg!("result: {:?}", result);
 
+    if root == result {
+        msg!("Verified");
+    }
+
     Ok(())
+}
+
+pub fn set_whitelist_account(account: String) {
+    unsafe {
+        IS_WHITELISTED = Some(account);
+    }
+}
+
+pub fn verify_proof(tree: MerkleTree) -> [u8; 32] {
+    let account = get_whitelist_account();
+    print!("vcvc{:?}", account);
+    let mut leaf = [0u8; 32];
+    hash_it(account.as_bytes(), &mut leaf);
+    let proof = tree.make_proof(leaf);
+    msg!("Proof: {:?}", proof);
+
+    tree.check_proof(proof, leaf)
+}
+
+fn get_whitelist_account() -> String {
+    unsafe {
+        match &IS_WHITELISTED {
+            Some(account) => account.clone(),
+            None => String::new(),
+        }
+    }
 }
